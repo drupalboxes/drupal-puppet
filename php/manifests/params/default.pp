c3aed707d81d788339e697fdf02f47b9012e8968
Rob Knight <mail@robknight.org.uk>
2011-09-03 18:34:56 -0400

    Recent versions of APC bork if 'M' character included in memory limit

--- php/manifests/params/default.pp
+++ php/manifests/params/default.pp
@@ -3,6 +3,6 @@ class php::params::default {
   $package        = 'php'
   $package_common = "$package-common"
   $php_ini        = '/etc/php.ini'
-  $memory_limit   = '128M'
+  $memory_limit   = '128'
   $php_dir        = '/etc/php.d'
 }
