54fe60465d9e68a7d0adc9347e54ade669690aad
Rob Knight <mail@robknight.org.uk>
2011-09-08 16:36:07 +0100

    Fixed some missing parameters in php::params

--- php/manifests/params.pp
+++ php/manifests/params.pp
@@ -1,10 +1,13 @@
 
 class php::params(
-  $package           = $php::params::default::package,
-  $package_common    = $php::params::default::package_common,
-  $php_ini           = $php::params::default::php_ini,
-  $memory_limit      = $php::params::default::memory_limit,
-  $php_dir           = $php::params::default::php_dir
+  $package              = $php::params::default::package,
+  $package_common       = $php::params::default::package_common,
+  $php_ini              = $php::params::default::php_ini,
+  $memory_limit         = $php::params::default::memory_limit,
+  $php_dir              = $php::params::default::php_dir,
+  $upload_max_filesize  = $php::params::default::upload_max_filesize,
+  $post_max_size        = $php::params::default::post_max_size,
+  $display_errors       = $php::params::default::display_errors
 ) inherits php::params::default {
 
 }
