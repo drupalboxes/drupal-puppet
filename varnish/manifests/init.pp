b989bf5e664ff398280f53a91cab21d771c85dd7
Rob Knight <mail@robknight.org.uk>
2011-09-08 16:35:46 +0100

    Made purge acl IP addresses configurable

--- varnish/manifests/init.pp
+++ varnish/manifests/init.pp
@@ -23,6 +23,7 @@ class varnish::config {
   $default_vcl        = $varnish::params::default_vcl
   $listen_port        = $varnish::params::listen_port
   $admin_listen_port  = $varnish::params::admin_listen_port
+  $purge_ips          = $varnish::params::purge_ips
 
   file {
     'varnish/default.vcl':
