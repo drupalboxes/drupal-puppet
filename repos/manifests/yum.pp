621fb25b496c3eca844758851ac55a809974cf6c
Rob Knight <mail@robknight.org.uk>
2011-09-16 23:15:39 +0100

    Added a define for yum repos which also includes optional GPG keys

---
+++ repos/manifests/yum.pp
@@ -0,0 +1,47 @@
+
+define repos::yum(
+  $baseurl = absent,
+  $descr = absent,
+  $enabled = 0,
+  $exclude = absent,
+  $failovermethod = absent,
+  $gpgcheck = 0,
+  $gpgkey_path = absent,
+  $gpgkey_source = absent,
+  $includepkgs = absent,
+  $keepalive = 0,
+  $mirrorlist = absent,
+  $priority = 99,
+  $proxy = absent,
+  $proxy_password = absent,
+  $proxy_username = absent,
+  $reponame = ''
+) {
+
+  if $gpgkey != absent and $gpgkey_source != absent {
+    file { "repos/yum/${name}/gpgkey":
+      path    => $gpgkey_path,
+      source  => $gpgkey_source,
+      before  => Yumrepo["repos/yum/${name}"]
+    }
+  }
+  
+  yumrepo { "repos/yum/${name}":
+    baseurl         => $baseurl,
+    descr           => $descr,
+    enabled         => $enabled,
+    exclude         => $exclude,
+    failovermethod  => $failovermethod,
+    gpgcheck        => $gpgcheck,
+    gpgkey          => $gpgkey ? { absent => absent, default => "file://${gpgkey_path}" },
+    includepkgs     => $includepkgs,
+    keepalive       => $keepalive,
+    mirrorlist      => $mirrorlist,
+    priority        => $priority,
+    proxy           => $proxy,
+    proxy_password  => $proxy_password,
+    proxy_username  => $proxy_username,
+    name            => $reponame
+  }
+  
+}
