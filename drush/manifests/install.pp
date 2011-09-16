65638b4ac771ae2c6bf3af3029c4e11a8a245bf6
Rob Knight <mail@robknight.org.uk>
2011-09-16 23:16:47 +0100

    Made Drush package dependent on Drupalboxes repo

--- drush/manifests/install.pp
+++ drush/manifests/install.pp
@@ -1,7 +1,9 @@
 
 class drush::install {
+  realize Repos::Yum['base/el/repos/drupalboxes']
   package { 'drush':
     ensure   => $drush::version,
-    name     => $drush::package
+    name     => $drush::package,
+    require  => Repos::Yum['base/el/repos/drupalboxes']
   }
 }
