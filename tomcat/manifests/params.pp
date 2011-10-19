14ce4b26a3cda5c119b7e032f3d5e63059d2605f
Rob Knight <mail@robknight.org.uk>
2011-10-19 16:44:43 +0100

    Set tomcat6 as default tomcat

--- tomcat/manifests/params.pp
+++ tomcat/manifests/params.pp
@@ -1,8 +1,8 @@
 
 class tomcat::params(
-  $package    = 'tomcat5',
-  $service    = 'tomcat5',
-  $installdir = '/var/lib/tomcat5'
+  $package    = 'tomcat6',
+  $service    = 'tomcat6',
+  $installdir = '/var/lib/tomcat6'
 ) {
 
 }
