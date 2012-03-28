100f4365fc4f147b54e6bfde6e924a642e2eaad5
Rob Knight <mail@robknight.org.uk>
2012-03-28 16:31:51 +0100

    Updated settings to Drupal 7 and changed focus toward site configuration

--- drupal/manifests/params.pp
+++ drupal/manifests/params.pp
@@ -1,10 +1,7 @@
 
 class drupal::params(
-  $install_path   = '/var/lib/drupal',
   $file_owner	    = 'drupal',
   $file_group	    = 'drupal',
-  $platforms_dir  = 'platforms',
-  $sites_dir      = 'sites'
 ) {
 
 }
--- drupal/manifests/site.pp
+++ drupal/manifests/site.pp
@@ -1,49 +1,48 @@
-#
-# A Drupal site depends on a Drupal platform. The platform is a codebase which
-# includes Drupal core plus any added extras (which may include modules in
-# an install profile or sites/all/modules).
-#
 
 define drupal::site(
-  $platform,
   $db_name,
   $db_user,
   $db_password,
   $db_host,
-  $filepath,
+  $drupal_root,
   $conf = { },
-  $files = absent,
-  $modules = absent,
-  $themes = absent,
-  $libraries = absent,
   $url = null,
   $aliases = []
 ) {
-  $base = $drupal::params::install_path
-  $site = "${base}/${drupal::params::sites_dir}/${name}"
-  $platform_path = "${base}/${drupal::params::platforms_dir}/${platform}"
-  
+
   $primary_url = $url ? {
     null    => $name,
     default => $url
   }
-  
-  $site_link = "${platform_path}/sites/${primary_url}"
-  
-  file { "${name}_site_dir":
-    ensure  => directory,
-    path    => $site
+
+  $site = "${drupal_root}/sites/${primary_url}"
+
+  # needed due to Puppet bug: http://projects.puppetlabs.com/issues/9322
+  Exec {
+    path => "/bin:/sbin:/usr/bin:/usr/sbin"
   }
 
-  file { "${name}_site_link":
-    ensure  => link,
-    path    => $site_link,
-    target  => $site
+  exec { "${name}_drupal_root":
+    command => "mkdir -p ${drupal_root}/sites",
+    creates => "${drupal_root}/sites"
   }
-  
+
+  file { "${name}_site_dir":
+    ensure  => directory,
+    path    => $site,
+    require => Exec["${name}_drupal_root"]
+  }
+
+  file { "${name}_site_files_dir":
+    ensure  => directory,
+    path    => "${site}/files",
+    require => File["${name}_site_dir"]
+    # todo: permissions
+  }
+
   drupal::site_alias { $aliases:
-    platform  => $platform,
-    site      => $name
+    drupal_root => $drupal_root,
+    site        => $name
   }
 
   file { "${name}_settings":
@@ -52,8 +51,8 @@ define drupal::site(
     content => template("drupal/settings.php.erb")
   }
 
-  apache::vhost {$primary_url:
-    documentroot => $platform_path,
+  apache::vhost { $primary_url:
+    documentroot => $drupal_root,
     port         => 80
   }
 }
