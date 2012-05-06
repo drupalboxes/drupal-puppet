ec11820c07bf9fdc65f0d0892b178517feb1a4cc
Rob Knight <mail@robknight.org.uk>
2012-05-06 21:07:36 +0100

    Made string interpolation a bit clearer

--- php/manifests/extension.pp
+++ php/manifests/extension.pp
@@ -4,7 +4,7 @@ define php::extension {
   include php
   
   package { "php-${name}":
-    name   => "$php::params::package-${name}",
+    name   => "${php::params::package}-${name}",
     ensure => present,
     notify => Class['apache::service']
   }
