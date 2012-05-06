12fb584f43ea9becec0067d597108371dc8b27c4
Rob Knight <mail@robknight.org.uk>
2012-05-06 21:05:32 +0100

    Removed 'default' php params class

--- php/manifests/params.pp
+++ php/manifests/params.pp
@@ -1,14 +1,14 @@
 
 class php::params(
-  $package              = $php::params::default::package,
-  $package_common       = $php::params::default::package_common,
-  $php_ini              = $php::params::default::php_ini,
-  $memory_limit         = $php::params::default::memory_limit,
-  $php_dir              = $php::params::default::php_dir,
-  $upload_max_filesize  = $php::params::default::upload_max_filesize,
-  $post_max_size        = $php::params::default::post_max_size,
-  $display_errors       = $php::params::default::display_errors,
-  $extension_dir	= $php::params::default::extension_dir
-) inherits php::params::default {
+  $package              = 'php',
+  $package_common       = "${package}-common",
+  $php_ini              = '/etc/php.ini',
+  $memory_limit         = '128',
+  $php_dir              = '/etc/php.d',
+  $upload_max_filesize  = '8M',
+  $post_max_size        = '8M',
+  $display_errors       = 'Off',
+  $extension_dir	= '/usr/lib64/php/modules'
+) {
 
 }
